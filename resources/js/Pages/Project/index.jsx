77660cb8a326035260c2cf361c2cd7ba58f519d8
Chad Chapman <chad.chapman2010@gmail.com>
2024-11-07 20:39:44 -0500

    Begin building view

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -1,6 +1,14 @@
-export default function Index() {
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+
+export default function Index({auth}) {
     return (
-        <>
-        </>
+        <AuthenticatedLayout>
+            user={auth.user}
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                    Dashboard
+                </h2>
+            }
+        </AuthenticatedLayout>
     )
 }
